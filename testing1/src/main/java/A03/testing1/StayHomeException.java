e43755ad4dff34f5de9a5342f589ac61fe7a16a0
AlejandroGalvezUCLM <92250656+AlejandroGalvezUCLM@users.noreply.github.com>
2021-12-22 18:28:40 +0100

    Add files via upload

---
+++ testing1/src/main/java/A03/testing1/StayHomeException.java
@@ -0,0 +1,7 @@
+package A03.testing1;
+
+public class StayHomeException extends Exception{
+	public StayHomeException(String msg) {
+		super(msg);
+	}
+}
---
+++ testing1/src/test/java/A03/testing1/RecommendationTestEachUse.java
@@ -0,0 +1,72 @@
+package A03.testing1;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class RecommendationTestEachUse {
+
+	@Test
+	public void testPersonGetRecommendationEU1() throws StayHomeException {
+		Person p = new Person(true, false, false, true, true);
+		Weather w = new Weather(-1, 0.14, true);
+		Place pl = new Place(true);
+		Recommendation r = new Recommendation(w, p, pl);
+		ArrayList<String> actRecommended = new ArrayList<String>();
+		actRecommended.add("Stay at home");
+		assertEquals(r.getRecommendation(), actRecommended);
+		
+	}
+	
+	@Test
+	public void testPersonGetRecommendationEU2() throws StayHomeException {
+		Person p = new Person(true, false, false, true, true);
+		Weather w = new Weather(-2147483647, 0.14, false);
+		Place pl = new Place(false);
+		Recommendation r = new Recommendation(w, p, pl);
+		ArrayList<String> actRecommended = new ArrayList<String>();
+		actRecommended.add("Go skiing");
+		assertEquals(r.getRecommendation(), actRecommended);
+		
+	}
+	
+	@Test
+	public void testPersonGetRecommendationEU3() throws StayHomeException {
+		Person p = new Person(true, false, false, true, true);
+		Weather w = new Weather(14, 0.15, false);
+		Place pl = new Place(false);
+		Recommendation r = new Recommendation(w, p, pl);
+		ArrayList<String> actRecommended = new ArrayList<String>();
+		actRecommended.add("Go hiking");
+		assertEquals(r.getRecommendation(), actRecommended);
+		
+	}
+	
+	@Test
+	public void testPersonGetRecommendationEU4() throws StayHomeException {
+		Person p = new Person(true, false, false, true, true);
+		Weather w = new Weather(16, 0.16, false);
+		Place pl = new Place(false);
+		Recommendation r = new Recommendation(w, p, pl);
+		ArrayList<String> actRecommended = new ArrayList<String>();
+		actRecommended.add("Go Sightseeing");
+		assertEquals(r.getRecommendation(), actRecommended);
+		
+	}
+
+	@Test
+	public void testPersonGetRecommendationEU5() throws StayHomeException {
+		Person p = new Person(true, false, false, true, true);
+		Weather w = new Weather(34, 0.61, false);
+		Place pl = new Place(false);
+		Recommendation r = new Recommendation(w, p, pl);
+		ArrayList<String> actRecommended = new ArrayList<String>();
+		actRecommended.add("Go Beering");
+		actRecommended.add("Go to the swimming pool");
+		assertEquals(r.getRecommendation(), actRecommended);
+		
+	}
+
+}
